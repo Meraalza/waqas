92d9ea5e1dd67944eb0a6e4d53c0882aca0e82c3
Meraalza <meraalshah447@gmail.com>
2025-09-01 19:03:24 +0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,6 +10,7 @@ const navItems = [
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
+  { name: "Blog", href: "/blog" },
   { name: "Experience", href: "#experience" },
   { name: "Contact", href: "#contact" },
 ]
